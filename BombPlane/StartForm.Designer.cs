7ab48bc7f9d0b8dab75587a657250ae755d48e72
toposyz <2335483715@qq.com>
2022-11-05 10:17:00 +0800

    update startform

--- BombPlane/StartForm.Designer.cs
+++ BombPlane/StartForm.Designer.cs
@@ -34,6 +34,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.LocalPanel = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.LocalPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Title
@@ -43,7 +50,7 @@
             this.Title.BackColor = System.Drawing.Color.Transparent;
             this.Title.Font = new System.Drawing.Font("华文新魏", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Title.ForeColor = System.Drawing.Color.OrangeRed;
-            this.Title.Location = new System.Drawing.Point(200, 77);
+            this.Title.Location = new System.Drawing.Point(15, 28);
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(297, 68);
             this.Title.TabIndex = 0;
@@ -53,28 +60,29 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Font = new System.Drawing.Font("汉仪南宫体简", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(256, 191);
+            this.button1.Location = new System.Drawing.Point(52, 127);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(220, 60);
             this.button1.TabIndex = 1;
             this.button1.Text = "联机对战";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Font = new System.Drawing.Font("汉仪南宫体简", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(256, 274);
+            this.button2.Location = new System.Drawing.Point(52, 208);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(220, 60);
             this.button2.TabIndex = 2;
-            this.button2.Text = "AI对战";
+            this.button2.Text = "本地对战";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -82,9 +90,9 @@
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.Font = new System.Drawing.Font("汉仪南宫体简", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(256, 359);
+            this.button3.Location = new System.Drawing.Point(52, 291);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(220, 60);
@@ -97,9 +105,9 @@
             // 
             this.CloseButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.CloseButton.BackColor = System.Drawing.Color.Transparent;
-            this.CloseButton.Font = new System.Drawing.Font("汉仪南宫体简", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.CloseButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.CloseButton.Location = new System.Drawing.Point(256, 444);
+            this.CloseButton.Location = new System.Drawing.Point(52, 370);
             this.CloseButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CloseButton.Name = "CloseButton";
             this.CloseButton.Size = new System.Drawing.Size(220, 60);
@@ -108,6 +116,61 @@
             this.CloseButton.UseVisualStyleBackColor = false;
             this.CloseButton.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(24, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 60);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "看海模式";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(24, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 61);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "人机模式";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // LocalPanel
+            // 
+            this.LocalPanel.BackColor = System.Drawing.Color.Transparent;
+            this.LocalPanel.Controls.Add(this.button6);
+            this.LocalPanel.Controls.Add(this.button5);
+            this.LocalPanel.Controls.Add(this.button4);
+            this.LocalPanel.Location = new System.Drawing.Point(282, 188);
+            this.LocalPanel.Name = "LocalPanel";
+            this.LocalPanel.Size = new System.Drawing.Size(225, 325);
+            this.LocalPanel.TabIndex = 6;
+            this.LocalPanel.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(24, 213);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(162, 60);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "返回";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.Transparent;
+            this.TopPanel.Controls.Add(this.Title);
+            this.TopPanel.Controls.Add(this.button1);
+            this.TopPanel.Controls.Add(this.CloseButton);
+            this.TopPanel.Controls.Add(this.button2);
+            this.TopPanel.Controls.Add(this.button3);
+            this.TopPanel.Location = new System.Drawing.Point(204, 62);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(322, 471);
+            this.TopPanel.TabIndex = 7;
+            // 
             // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -115,17 +178,16 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(751, 556);
-            this.Controls.Add(this.CloseButton);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.Title);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.LocalPanel);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "StartForm";
             this.Text = "Bomb Planes";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.LocalPanel.ResumeLayout(false);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -136,6 +198,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button CloseButton;
+        private Button button5;
+        private Button button4;
+        private Panel LocalPanel;
+        private Button button6;
+        private Panel TopPanel;
     }
 }
 
--- BombPlane/StartForm.cs
+++ BombPlane/StartForm.cs
@@ -14,24 +14,48 @@ namespace BombPlane
         {
 
         }
-
-        private void button4_Click(object sender, EventArgs e)
+        //联机对战按钮
+        private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
-        }
 
+        }
+        //本地对战按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TopPanel.Visible = false;
+            LocalPanel.Visible = true;
+            /*Form Gameform1 = new GameForm(this);
+            this.Hide();
+            Gameform1.Show();*/
+        }
+        //帮助按钮
         private void button3_Click(object sender, EventArgs e)
         {
             Form Helpform = new HelpForm(this);
             this.Hide();
             Helpform.Show();
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        //退出按钮
+        private void button4_Click(object sender, EventArgs e)
         {
-            Form Gameform1 = new GameForm(this);
-            this.Hide();
-            Gameform1.Show();
+            this.Close();
         }
+        //人机模式按钮
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        //看海模式按钮
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+        //本地模式返回上级菜单
+        private void button6_Click(object sender, EventArgs e)
+        {
+            LocalPanel.Visible = false;
+            TopPanel.Visible = true;
+        }
+        
     }
 }
