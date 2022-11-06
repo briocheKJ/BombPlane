9b28c2f014491ad9cc473cf30db49311ae29e3dc
unknown <maxwei.wzj@gmail.com>
2022-11-06 10:52:03 +0800

    Added Server

---
+++ BombPlane_Server/Msg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombPlane
+{
+    abstract class Msg
+    {
+        public string msgType { get; }
+
+        public Msg() { }
+        public Msg(string msgType) { this.msgType = msgType; }
+        public byte[] getHead()
+        {
+            return Encoding.UTF8.GetBytes(msgType);
+        }
+        public void Send(Socket socket)
+        {
+            socket.Send(Encoding.UTF8.GetBytes(this.msgType));
+            socket.Send(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this)));
+        }
+        static public Msg Receive(Socket socket)
+        {
+            byte[] bytes = new byte[1024];
+            int len = socket.Receive(bytes);
+            string type = Encoding.UTF8.GetString(bytes, 0, len);
+            len = socket.Receive(bytes);
+            string jsonString = Encoding.UTF8.GetString(bytes, 0, len);
+            if (type == "ReadyMsg")
+            {
+                Msg msg = (Msg)(JsonConvert.DeserializeObject<ReadyMsg>(jsonString));
+                return msg;
+            }
+            return null;
+        }
+    }
+    class ReadyMsg : Msg
+    {
+        String text = "ack";
+        public ReadyMsg() : base("ReadyMsg")
+        {
+        }
+        public void print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ BombPlane_Server/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombPlane
+{
+    class Server
+    {
+        static string ip = "127.0.0.1";
+        static int port = 1111;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("服务器启动.....");
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            EndPoint point = new IPEndPoint(IPAddress.Parse(ip), port);
+            server.Bind(point);
+            server.Listen(100);
+
+            Socket socket1 = server.Accept();
+            Console.WriteLine("a client connect.....");
+
+            Socket socket2 = server.Accept();
+            Console.WriteLine("a client connect.....");
+
+            (new ReadyMsg()).Send(socket1);
+            (new ReadyMsg()).Send(socket2);
+
+            Transmitor trans1 = new Transmitor(socket1, socket2);
+            Transmitor trans2 = new Transmitor(socket2, socket1);
+
+            Thread thread1 = new Thread(trans1.Runner);
+            Thread thread2 = new Thread(trans2.Runner);
+
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+    class Transmitor
+    {
+        public Socket socket1;
+        public Socket socket2;
+        public Transmitor(Socket socket1, Socket socket2)
+        {
+            this.socket1 = socket1;
+            this.socket2 = socket2;
+        }
+        public void Runner()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                int len = socket1.Receive(bytes);
+                string content = Encoding.UTF8.GetString(bytes, 0, len);
+                socket1.Send(Encoding.UTF8.GetBytes(content));
+            }
+        }
+    }
+    
+}
