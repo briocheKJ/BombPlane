339640efc08e6ed6fd028b12a2e6626625949663
LiuKangJie <1982961285@qq.com>
2022-11-12 13:03:34 +0800

    ai entroy

--- BombPlane/AIPlayer_Entropy.cs
+++ BombPlane/AIPlayer_Entropy.cs
@@ -153,8 +153,8 @@ namespace BombPlane
         public override int TakeAction(int[][] state)
         {
             Random rand = new Random();
-            int act = -1,cnt=0,res=0;
-            double mm=0;
+            int act = -3,cnt=0,res=0;
+            double mm=-1;
             for (int j = 0; j < tot; j++) vis[j] = false;
             for (int i = 0; i < 100; i++)
             {
@@ -174,8 +174,9 @@ namespace BombPlane
                     if (state[i / 10][i % 10] == -1 && store[res][i / 10][i % 10] == 2)
                         return i;
                 }
-                return -1;
+                return -2;
             }
+            double[] entropy = new double[100];
             for (int i=0;i<100;i++)
             {
                 if (state[i / 10][i % 10] != -1) continue;
@@ -188,16 +189,16 @@ namespace BombPlane
                     if (store[j][i / 10][i % 10] == 1) aim_body++;
                     if (store[j][i / 10][i % 10] == 2) aim_head++;
                 }
-                double entropy=-blank/sum*Math.Log(blank / sum)
-                    + aim_head /sum* (1.0-  Math.Log(aim_head / sum))
-                    - aim_body / sum * Math.Log(aim_body / sum);
-                if (entropy > mm)
+                entropy[i] = 0;
+                if (blank != 0) entropy[i] += -blank / sum * Math.Log(blank / sum);
+                if (aim_head != 0) entropy[i] += aim_head / sum * (1.0 - Math.Log(aim_head / sum));
+                if(aim_body!=0)entropy[i]+= -aim_body / sum * Math.Log(aim_body / sum);
+                if (entropy[i] > mm)
                 { 
-                    mm = entropy;
+                    mm = entropy[i];
                     act = i;
                 }
             }
-
             Thread.Sleep(1000);
 
             return act;
